192925f8787883af6c7e2e31c839a773f4cd5f3d
eVasquez97 <lavairelavaire@gmail.com>
2023-08-13 16:12:28 -0600

    wip: adjust styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Eduardo Vásquez</h1>
+    <main className="flex min-h-screen flex-col items-center p-4 mx-auto max-w-[1024px]">
+      <div className="bg-white rounded-sm p-4 shadow-xl w-full">
+        <h1>Eduardo Vásquez</h1>
+      </div>
     </main>
   );
 }
