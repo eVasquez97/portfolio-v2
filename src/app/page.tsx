81bba8b574173886f70b743f556210ef8b998764
eVasquez97 <lavairelavaire@gmail.com>
2023-09-30 21:15:14 -0600

    reacftor(Link): spacing and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
-import projects from "../utils/projects.json";
 import { ProjectCard } from "@/components/organisms";
 import { variableText } from "@/utils/constants";
+import Link from "next/link";
+import projects from "../utils/projects.json";
 
 export default function Home() {
   const showCase = projects?.slice(0, 4);
-  const minimalLink = `${variableText} italic font-semibold hover:underline`;
+  const minimalLink = `${variableText} italic font-semibold hover:underline mx-1`;
 
   return (
     <div className="flex flex-col w-full">
       <p className={`${variableText} mb-5`}>
         I&apos;m a{" "}
-        <a className={`${minimalLink}`} href="/services">
+        <Link className={`${minimalLink}`} href="/services">
           full stack developer{" "}
-        </a>{" "}
+        </Link>{" "}
         who enjoys tackling challenging problems and delivering quality
         products. I consider myself a React JS master and Python & TypeScript
         enthusiast. My most recent projects include:
@@ -36,20 +37,20 @@ export default function Home() {
       </div>
       <p className={`${variableText} my-8`}>
         And that&apos;s not all, you may check{" "}
-        <a className={`${minimalLink}`} href="/projects">
+        <Link className={`${minimalLink}`} href="/projects">
           all projects{" "}
-        </a>{" "}
+        </Link>{" "}
         and contributions. I&apos;m always eager to learn new skills and
         technologies to enhance my professional growth. On my free time I enjoy
         cooking, reading, exercising, and programming cause{" "}
         <span className="italic">this is the way.</span>I also provide various{" "}
-        <a className={`${minimalLink}`} href="/services">
+        <Link className={`${minimalLink}`} href="/services">
           services{" "}
-        </a>
+        </Link>
         on the IT sector; but if you wanna know more{" "}
-        <a className={`${minimalLink}`} href="/about">
+        <Link className={`${minimalLink}`} href="/about">
           about me{" "}
-        </a>
+        </Link>
         , check the page or reach out via any of the links below.
       </p>
     </div>
