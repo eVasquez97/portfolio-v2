0ba91df90a03695f51753d48b678a58f2351d8eb
eVasquez97 <lavairelavaire@gmail.com>
2025-11-15 21:55:53 -0600

    fix: columns display

--- src/components/molecules/ShowCase.tsx
+++ src/components/molecules/ShowCase.tsx
@@ -10,7 +10,7 @@ export default function ShowCase({ items, columns }: ShowCaseType) {
   const cols = columns ? `grid-cols-${columns.toString()}` : "md:grid-cols-3";
 
   return (
-    <div className={`grid ${cols} gap-2`}>
+    <div className={`grid ${cols} gap-2 grid-cols-1 w-full`}>
       {items.map((itm) => {
         return (
           <div className="flex w-full bg-cover bg-no-repeat" key={itm}>
